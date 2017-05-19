74fd90cbd8119d71c575d54c874a3924cf304751
SpyCheese <mikle98@yandex.ru>
2017-05-19 21:03:22 +0300

    Copied dz2 to dz3

---
+++ dz3/bigint/CMakeFiles/big_integer_testing.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/spycheese/Documents/itmo/cpp/bigint/big_integer.cpp" "/home/spycheese/Documents/itmo/cpp/bigint/CMakeFiles/big_integer_testing.dir/big_integer.cpp.o"
+  "/home/spycheese/Documents/itmo/cpp/bigint/big_integer_testing.cpp" "/home/spycheese/Documents/itmo/cpp/bigint/CMakeFiles/big_integer_testing.dir/big_integer_testing.cpp.o"
+  "/home/spycheese/Documents/itmo/cpp/bigint/gtest/gtest-all.cc" "/home/spycheese/Documents/itmo/cpp/bigint/CMakeFiles/big_integer_testing.dir/gtest/gtest-all.cc.o"
+  "/home/spycheese/Documents/itmo/cpp/bigint/gtest/gtest_main.cc" "/home/spycheese/Documents/itmo/cpp/bigint/CMakeFiles/big_integer_testing.dir/gtest/gtest_main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ dz3/bigint/big_integer.h
@@ -0,0 +1,99 @@
+#ifndef BIG_INTEGER_H
+#define BIG_INTEGER_H
+
+#include <iostream>
+#include <string>
+
+class big_integer
+{
+public:
+	big_integer();
+	big_integer(big_integer const & b);
+	big_integer(big_integer && b);
+	explicit big_integer(std::string const & s);
+	big_integer(int b);
+	big_integer(uint32_t b);
+
+	~big_integer();
+
+	void swap(big_integer & b);
+	big_integer & operator = (big_integer const & b);
+	big_integer & operator = (big_integer && b);
+
+	big_integer & operator += (big_integer const & b);
+	big_integer & operator -= (big_integer const & b);
+	big_integer & operator *= (big_integer const & b);
+	big_integer & operator /= (big_integer const & b);
+	big_integer & operator %= (big_integer const & b);
+	big_integer & operator &= (big_integer const & b);
+	big_integer & operator |= (big_integer const & b);
+	big_integer & operator ^= (big_integer const & b);
+	big_integer & operator <<= (int b);
+	big_integer & operator >>= (int b);
+
+	big_integer operator + () const;
+	big_integer operator - () const;
+	big_integer operator ~ () const;
+
+	big_integer & operator ++ ();
+	big_integer & operator -- ();
+	big_integer operator ++ (int);
+	big_integer operator -- (int);
+
+	friend big_integer operator + (big_integer a, big_integer const & b);
+	friend big_integer operator - (big_integer a, big_integer const & b);
+	friend big_integer operator * (big_integer const & a, big_integer const & b);
+	friend big_integer operator / (big_integer a, big_integer const & b);
+	friend big_integer operator % (big_integer a, big_integer const & b);
+	friend big_integer operator & (big_integer a, big_integer const & b);
+	friend big_integer operator | (big_integer a, big_integer const & b);
+	friend big_integer operator ^ (big_integer a, big_integer const & b);
+	friend big_integer operator << (big_integer a, int b);
+	friend big_integer operator >> (big_integer a, int b);
+
+	friend bool operator == (big_integer const & a, big_integer const & b);
+	friend bool operator != (big_integer const & a, big_integer const & b);
+	friend bool operator < (big_integer const & a, big_integer const & b);
+	friend bool operator > (big_integer const & a, big_integer const & b);
+	friend bool operator <= (big_integer const & a, big_integer const & b);
+	friend bool operator >= (big_integer const & a, big_integer const & b);
+
+	friend std::string to_string(big_integer a);
+
+private:
+	size_t size;
+	uint32_t * data;
+	bool neg;
+
+	void resize(size_t nsize);
+	void shrink();
+
+	void shiftL(size_t b);
+	void divModImpl(big_integer const & b, big_integer & r);
+	void subShifted(big_integer const & b, size_t shift);
+	bool lessShifted(big_integer const & b, size_t shift);
+};
+
+big_integer operator + (big_integer a, big_integer const & b);
+big_integer operator - (big_integer a, big_integer const & b);
+big_integer operator * (big_integer const & a, big_integer const & b);
+big_integer operator / (big_integer a, big_integer const & b);
+big_integer operator % (big_integer a, big_integer const & b);
+big_integer operator & (big_integer a, big_integer const & b);
+big_integer operator | (big_integer a, big_integer const & b);
+big_integer operator ^ (big_integer a, big_integer const & b);
+big_integer operator << (big_integer a, int b);
+big_integer operator >> (big_integer a, int b);
+
+bool operator == (big_integer const & a, big_integer const & b);
+bool operator != (big_integer const & a, big_integer const & b);
+bool operator < (big_integer const & a, big_integer const & b);
+bool operator > (big_integer const & a, big_integer const & b);
+bool operator <= (big_integer const & a, big_integer const & b);
+bool operator >= (big_integer const & a, big_integer const & b);
+
+std::string to_string(big_integer a);
+std::istream & operator >> (std::istream & in, big_integer & a);
+std::ostream & operator << (std::ostream & out, big_integer const & a);
+
+#endif // BIG_INTEGER_H
